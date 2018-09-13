9a91542596790485ec39bc4a4505f6726e38efb4
ElvisPressB <43243748+ElvisPressB@users.noreply.github.com>
2018-09-13 08:45:33 -0700

    logic changes, var to constants
    changed some of the logic, checking cooldowns after the command is established for instance, and changed cooldown variables

--- thatbot.js
+++ thatbot.js
@@ -35,13 +35,13 @@ if (typeof Cooldowns !== 'object') {
 // end create various objects in memory
 
 // various command cooldown variables
-var discordCD = 15;
-var hypeCD = 5;
-var instaCD = 15;
-var lurkCD = 5;
-var primeCD = 30;
-var raidCD = 5;
-var soCD = 5;
+const discordCD = 15;
+const hypeCD = 5;
+const instaCD = 15;
+const lurkCD = 5;
+const primeCD = 30;
+const raidCD = 5;
+const soCD = 5;
 
 function subSeconds(numSeconds) {
 		var time = new Date();
@@ -98,74 +98,74 @@ client.on('chat', function(channel, user, message, self) {
 		Cooldowns.color = 1;
 	}
 
-	if (subSeconds(discordCD) >= Cooldowns.discord) { // checks adjusted Date() vs discord command cooldown 
-		if (message === "!discord") { // if the cooldown is up the discord command is listened to
+	if (message === "!discord") { // if the cooldown is up the discord command is listened to
+		if (subSeconds(discordCD) >= Cooldowns.discord) { // checks adjusted Date() vs discord command cooldown
 			client.say("ElvisPressB", "@" + user['display-name'] + " , the PressB community Discord: https://discord.gg/x3HmECn");
 			Cooldowns.discord = new Date(); // the discord cooldown is refreshed with the current Date()
+		} else {
+			console.log("Discord cooldown not up"); // otherwise we get a console log that the cooldown isn't up
 		}
-	} else {
-		console.log("Discord cooldown not up"); // otherwise we get a console log that the cooldown isn't up
 	}
 
-	if (subSeconds(hypeCD) >= Cooldowns.hype) {
-		if (message === "!hype") {
+	if (message === "!hype") {
+		if (subSeconds(hypeCD) >= Cooldowns.hype) {
 			client.say("ElvisPressB", "Squid1 Squid2 Squid2 Squid3 Squid2 Squid2 Squid4");
 			Cooldowns.hype = new Date();
+		} else {
+			console.log("Hype cooldown not up");
 		}
-	} else {
-		console.log("Hype cooldown not up");
-	}
+	} 
 
-	if (subSeconds(instaCD) >= Cooldowns.insta) {
-		if (message === "!insta") {
+	if (message === "!insta") {
+		if (subSeconds(instaCD) >= Cooldowns.insta) {
 			client.say("ElvisPressB", "@" + user['display-name'] + " , check out Elvis' weird pictures here! https://www.instagram.com/elvispressb/");
 			Cooldowns.insta = new Date();
+		} else {
+			console.log("Insta cooldown not up");
 		}
-	} else {
-		console.log("Insta cooldown not up");
 	}
 
-	if (subSeconds(lurkCD) >= Cooldowns.lurk) {
-		if (message === "!lurk") {
+	if (message === "!lurk") {
+		if (subSeconds(lurkCD) >= Cooldowns.lurk) {
 			client.action("ElvisPressB", "shows @" + user['display-name'] + " to a comfortable seat in the lurker section.");
 			Cooldowns.lurk = new Date();
+		} else {
+			console.log("Lurk cooldown not up");
 		}
-	} else {
-		console.log("Lurk cooldown not up");
 	}
 
-	if (subSeconds(primeCD) >= Cooldowns.prime) {
-		if (message === "!prime") {
+	if (message === "!prime") {
+		if (subSeconds(primeCD) >= Cooldowns.prime) {
 			client.say("ElvisPressB", "@" + user['display-name'] + " How to link your Twitch and Amazon Prime: https://help.twitch.tv/customer/portal/articles/2574978-how-to-link-your-amazon-account You can use your free prime sub to get a *sweet* savage emote! It's a great way to directly support Elvis!");
 			Cooldowns.prime = new Date();
+		} else {
+			console.log("Prime cooldown not up");
 		}
-	} else {
-		console.log("Prime cooldown not up");
 	}
 
-	if (subSeconds(raidCD) >= Cooldowns.raid) {
-		if (message === "!raid") {
+	if (message === "!raid") {
+		if (subSeconds(raidCD) >= Cooldowns.raid) {
 			client.say("ElvisPressB", "twitchRaid TombRaid twitchRaid TombRaid twitchRaid TombRaid twitchRaid TombRaid twitchRaid");
 			Cooldowns.raid = new Date();
+		} else {
+			console.log("Raid cooldown not up");
 		}
-	} else {
-		console.log("Raid cooldown not up");
 	}
 
-	if (user["user-type"] === "mod" || user.username === channel.replace("#", "")) {
-		if (subSeconds(soCD) >= Cooldowns.so) {
-			if (message) {
-				var soText = message;
-				var soEvent = soText.slice(0, 3);
-				if (soEvent === "!so") {
+	if (message) {
+		var soText = message;
+		var soEvent = soText.slice(0, 3);
+		if (soEvent === "!so") {
+			if (user["user-type"] === "mod" || user.username === channel.replace("#", "")) {
+				if (subSeconds(soCD) >= Cooldowns.so) {
 					var targetStreamer = soText.substr(4);
 					client.say("ElvisPressB", "Please go check out this amazing streamer at https://www.twitch.tv/" + targetStreamer + " and show them some love! <3 VoteYea <3");
 					Cooldowns.so = new Date();
+				} else {
+					console.log("SO cooldown not up");
 				}
 			}
-		} else {
-		console.log("SO cooldown not up");
-		}
+		} 
 	} 
 
 });
