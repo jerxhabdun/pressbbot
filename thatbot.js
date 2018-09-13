5b881139963dddb7e8281ee9749c1579c314c08b
ElvisPressB <43243748+ElvisPressB@users.noreply.github.com>
2018-09-13 10:43:48 -0700

    some serious restructuring
    lots of changes inc, vague description is vague

--- thatbot.js
+++ thatbot.js
@@ -98,74 +98,68 @@ client.on('chat', function(channel, user, message, self) {
 		Cooldowns.color = 1;
 	}
 
-	if (message === "!discord") { // if the cooldown is up the discord command is listened to
-		if (subSeconds(discordCD) >= Cooldowns.discord) { // checks adjusted Date() vs discord command cooldown
-			client.say("ElvisPressB", "@" + user['display-name'] + " , the PressB community Discord: https://discord.gg/x3HmECn");
-			Cooldowns.discord = new Date(); // the discord cooldown is refreshed with the current Date()
-		} else {
-			console.log("Discord cooldown not up"); // otherwise we get a console log that the cooldown isn't up
-		}
-	}
-
-	if (message === "!hype") {
-		if (subSeconds(hypeCD) >= Cooldowns.hype) {
-			client.say("ElvisPressB", "Squid1 Squid2 Squid2 Squid3 Squid2 Squid2 Squid4");
-			Cooldowns.hype = new Date();
-		} else {
-			console.log("Hype cooldown not up");
-		}
-	} 
-
-	if (message === "!insta") {
-		if (subSeconds(instaCD) >= Cooldowns.insta) {
-			client.say("ElvisPressB", "@" + user['display-name'] + " , check out Elvis' weird pictures here! https://www.instagram.com/elvispressb/");
-			Cooldowns.insta = new Date();
-		} else {
-			console.log("Insta cooldown not up");
-		}
-	}
-
-	if (message === "!lurk") {
-		if (subSeconds(lurkCD) >= Cooldowns.lurk) {
-			client.action("ElvisPressB", "shows @" + user['display-name'] + " to a comfortable seat in the lurker section.");
-			Cooldowns.lurk = new Date();
-		} else {
-			console.log("Lurk cooldown not up");
-		}
-	}
-
-	if (message === "!prime") {
-		if (subSeconds(primeCD) >= Cooldowns.prime) {
-			client.say("ElvisPressB", "@" + user['display-name'] + " How to link your Twitch and Amazon Prime: https://help.twitch.tv/customer/portal/articles/2574978-how-to-link-your-amazon-account You can use your free prime sub to get a *sweet* savage emote! It's a great way to directly support Elvis!");
-			Cooldowns.prime = new Date();
-		} else {
-			console.log("Prime cooldown not up");
-		}
-	}
-
-	if (message === "!raid") {
-		if (subSeconds(raidCD) >= Cooldowns.raid) {
-			client.say("ElvisPressB", "twitchRaid TombRaid twitchRaid TombRaid twitchRaid TombRaid twitchRaid TombRaid twitchRaid");
-			Cooldowns.raid = new Date();
-		} else {
-			console.log("Raid cooldown not up");
-		}
-	}
-
 	if (message) {
-		var soText = message;
-		var soEvent = soText.slice(0, 3);
-		if (soEvent === "!so") {
-			if (user["user-type"] === "mod" || user.username === channel.replace("#", "")) {
-				if (subSeconds(soCD) >= Cooldowns.so) {
-					var targetStreamer = soText.substr(4);
-					client.say("ElvisPressB", "Please go check out this amazing streamer at https://www.twitch.tv/" + targetStreamer + " and show them some love! <3 VoteYea <3");
-					Cooldowns.so = new Date();
-				} else {
-					console.log("SO cooldown not up");
+ 		
+ 		var words = message;
+		var command = words.slice(0, 1);
+		
+		if (command === "!") {
+			
+			switch (message) {
+				case "!raid":
+					if (subSeconds(raidCD) >= Cooldowns.raid) {
+						client.say("ElvisPressB", "twitchRaid TombRaid twitchRaid TombRaid twitchRaid TombRaid twitchRaid TombRaid twitchRaid");
+					Cooldowns.raid = new Date();
+					} else {
+						console.log("Raid cooldown not up");
+					}
+					break;
+				case "!prime":
+					if (subSeconds(primeCD) >= Cooldowns.prime) {
+						client.say("ElvisPressB", "@" + user['display-name'] + " How to link your Twitch and Amazon Prime: https://help.twitch.tv/customer/portal/articles/2574978-how-to-link-your-amazon-account You can use your free prime sub to get a *sweet* savage emote! It's a great way to directly support Elvis!");
+						Cooldowns.prime = new Date();
+					} else {
+						console.log("Prime cooldown not up");
+					}
+					break;
+				case "!lurk":
+					if (subSeconds(lurkCD) >= Cooldowns.lurk) {
+						client.action("ElvisPressB", "shows @" + user['display-name'] + " to a comfortable seat in the lurker section.");
+						Cooldowns.lurk = new Date();
+					} else {
+						console.log("Lurk cooldown not up");
+					}
+					break;
+				case "!insta":
+					if (subSeconds(instaCD) >= Cooldowns.insta) {
+						client.say("ElvisPressB", "@" + user['display-name'] + " , check out Elvis' weird pictures here! https://www.instagram.com/elvispressb/");
+						Cooldowns.insta = new Date();
+					} else {
+						console.log("Insta cooldown not up");
+					}
+					break;
+				case "!hype":
+					if (subSeconds(hypeCD) >= Cooldowns.hype) {
+						client.say("ElvisPressB", "Squid1 Squid2 Squid2 Squid3 Squid2 Squid2 Squid4");
+						Cooldowns.hype = new Date();
+					} else {
+						console.log("Hype cooldown not up");
+					}
+					break;
+				case "!discord": // discord command ?
+					if (subSeconds(discordCD) >= Cooldowns.discord) { // check the discord cooldown vs adjusted current time
+						client.say("ElvisPressB", "@" + user['display-name'] + " , the PressB community Discord: https://discord.gg/x3HmECn");
+						Cooldowns.discord = new Date(); // the discord cooldown is refreshed with the current Date()
+					} else {
+						console.log("Discord cooldown not up"); // otherwise we get a console log that the cooldown isn't up
+					}
+					break;
+				default:
+					client.say("ElvisPressB", "Sorry @" + user['display-name'] + " , but I don't recognize that command, please type !commands for a list of recognized commands.");
 				}
+
+
 			}
-		} 
-	} 
+		}
 
 });
