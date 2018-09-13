983fd9bf5ef909e5148733beb31ba7835285655b
jerxhabdun <43245251+jerxhabdun@users.noreply.github.com>
2018-09-13 15:35:41 -0400

    Some additional functions and json files

--- thatbot.js
+++ thatbot.js
@@ -1,8 +1,10 @@
 var tmi = require('tmi.js');
+var jerx = require('./coolfunctions/jerxfunctions.js');
 const login = require('./login');
 
 //This is a static load. Can update later to be asynch call so that channels can be added dynamically if needed
 var channelsFile = require('./channels.json');
+var admins = require('./admins.json');
 
 // TMI OPTIONS
 var options = {
@@ -28,6 +30,8 @@ client.connect();
 
 client.on('connected', function(address, port) {
 	console.log("Address: " + address + " Port: " + port);
+	//This is a test of the timer crap I'm working on
+	//var id = jerx.manageTimers((function() { console.log('This is an iterative test'); }), null, null, null);
 });
 // end set up, connect and console log client information
 
@@ -111,7 +115,7 @@ client.on('chat', function(channel, user, message, self) {
 			switch (message) {
 				case "!raid":
 					if (subSeconds(raidCD) >= Cooldowns.raid) {
-						client.say("ElvisPressB", "twitchRaid TombRaid twitchRaid TombRaid twitchRaid TombRaid twitchRaid TombRaid twitchRaid");
+						client.say(channel, "twitchRaid TombRaid twitchRaid TombRaid twitchRaid TombRaid twitchRaid TombRaid twitchRaid");
 					Cooldowns.raid = new Date();
 					} else {
 						console.log("Raid cooldown not up");
@@ -119,7 +123,7 @@ client.on('chat', function(channel, user, message, self) {
 					break;
 				case "!prime":
 					if (subSeconds(primeCD) >= Cooldowns.prime) {
-						client.say("ElvisPressB", "@" + user['display-name'] + " How to link your Twitch and Amazon Prime: https://help.twitch.tv/customer/portal/articles/2574978-how-to-link-your-amazon-account You can use your free prime sub to get a *sweet* savage emote! It's a great way to directly support Elvis!");
+						client.say(channel, "@" + user['display-name'] + " How to link your Twitch and Amazon Prime: https://help.twitch.tv/customer/portal/articles/2574978-how-to-link-your-amazon-account You can use your free prime sub to get a *sweet* savage emote! It's a great way to directly support Elvis!");
 						Cooldowns.prime = new Date();
 					} else {
 						console.log("Prime cooldown not up");
@@ -127,7 +131,7 @@ client.on('chat', function(channel, user, message, self) {
 					break;
 				case "!lurk":
 					if (subSeconds(lurkCD) >= Cooldowns.lurk) {
-						client.action("ElvisPressB", "shows @" + user['display-name'] + " to a comfortable seat in the lurker section.");
+						client.action(channel, "shows @" + user['display-name'] + " to a comfortable seat in the lurker section.");
 						Cooldowns.lurk = new Date();
 					} else {
 						console.log("Lurk cooldown not up");
@@ -135,7 +139,7 @@ client.on('chat', function(channel, user, message, self) {
 					break;
 				case "!insta":
 					if (subSeconds(instaCD) >= Cooldowns.insta) {
-						client.say("ElvisPressB", "@" + user['display-name'] + " , check out Elvis' weird pictures here! https://www.instagram.com/elvispressb/");
+						client.say(channel, "@" + user['display-name'] + " , check out Elvis' weird pictures here! https://www.instagram.com/elvispressb/");
 						Cooldowns.insta = new Date();
 					} else {
 						console.log("Insta cooldown not up");
@@ -143,7 +147,7 @@ client.on('chat', function(channel, user, message, self) {
 					break;
 				case "!hype":
 					if (subSeconds(hypeCD) >= Cooldowns.hype) {
-						client.say("ElvisPressB", "Squid1 Squid2 Squid2 Squid3 Squid2 Squid2 Squid4");
+						client.say(channel, "Squid1 Squid2 Squid2 Squid3 Squid2 Squid2 Squid4");
 						Cooldowns.hype = new Date();
 					} else {
 						console.log("Hype cooldown not up");
@@ -151,18 +155,34 @@ client.on('chat', function(channel, user, message, self) {
 					break;
 				case "!discord": // discord command ?
 					if (subSeconds(discordCD) >= Cooldowns.discord) { // check the discord cooldown vs adjusted current time
-						client.say("ElvisPressB", "@" + user['display-name'] + " , the PressB community Discord: https://discord.gg/x3HmECn");
+						client.say(channel, "@" + user['display-name'] + " , the PressB community Discord: https://discord.gg/x3HmECn");
 						Cooldowns.discord = new Date(); // the discord cooldown is refreshed with the current Date()
 					} else {
 						console.log("Discord cooldown not up"); // otherwise we get a console log that the cooldown isn't up
 					}
 					break;
 				default:
-					client.say("ElvisPressB", "Sorry @" + user['display-name'] + " , but I don't recognize that command, please type !commands for a list of recognized commands.");
+					client.say(channel, "Sorry @" + user['display-name'] + " , but I don't recognize that command, please type !commands for a list of recognized commands.");
 				}
 
 
 			}
 		}
 
+});
+
+client.on("whisper", function (from, userstate, message, self) {
+    // Don't listen to my own messages..
+    if (self) return;
+	
+	if(jerx.searchArray(from, admins)) {
+		var parsed = jerx.parse(message);
+		if(parsed.success){
+			client.whisper(from, "Command accepted: "+parsed.command+" with arg "+parsed.argument);
+		}
+		else {
+			client.whisper(from, "Error!");
+		}
+		return;
+	}
 });
